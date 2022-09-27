dee558ce75897699af0bc792f113c7e0f4bca398
Carlos Soto <carlos.soto@zitus.mx>
2022-09-27 14:51:18 -0500

    cors fixed

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import json
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from requests import post
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://itgeeky.github.io/portfolio-eng/",
+    "https://itgeeky.github.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/port_lead", tags=["LEADS"])
 def post_lead(data:str):
